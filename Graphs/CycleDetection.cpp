b42d4f331a0b79a46dc83766d95e615e6c096002
Pankaj Pandey <52026014+PanPey@users.noreply.github.com>
2021-07-26 18:27:16 +0530

    Create CycleDetection.cpp

---
+++ Graphs/CycleDetection.cpp
@@ -0,0 +1,38 @@
+
+// Using BFS
+bool bfsDetectCycle(int node, vector<int>adj[], int vis[]){
+      
+         queue<pair<int, int>>q;
+         vis[node]=1;
+         q.push({node, -1});
+
+         while(!q.empty()){
+           int curr = q.front().first;
+           int par = q.front().second;
+           q.pop();
+
+           for(auto it: adj[curr]){
+            if(!vis[it]){
+                vis[it]=1;
+                q.push({it, curr});
+            }
+            else if(par!=it){
+                return true;
+            }
+           }
+      }
+     return false;
+
+    }
+
+    bool checkCycle(vector<int>adj[], int vis[]){
+
+      for(int i=0; i<n; i++){
+        if(!vis[i]){
+            if(bfsDetectCycle(i, adj, vis))
+                return true;
+        }
+      }
+       return false;
+
+    }
