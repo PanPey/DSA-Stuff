867971e98eab7e63c5cf9e14f7ed56a0d3081455
Pankaj Pandey <52026014+PanPey@users.noreply.github.com>
2021-07-26 17:36:16 +0530

    Create BiPartiteCheck.cpp

---
+++ Graphs/BiPartiteCheck.cpp
@@ -0,0 +1,50 @@
+//Note: If there is an odd length cycle in a grapg then it can't be a bi paritite.
+// Otherwise it is.
+// Below is a bfs based sol...
+class Solution {
+public:
+    
+    bool bfs(int src, vector<vector<int>>& graph, int color[]){
+        
+        queue<int>q;
+        q.push(src);
+        color[src]=1;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            
+            for(auto it: graph[node]){
+                if(color[it]==-1){
+                    color[it]=1-color[node];
+                    q.push(it);
+                    
+                }
+                else if(color[it] == color[node]){
+                    return false;
+                }
+                    
+            }
+        }
+        
+        return true;
+    }
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        int color[n];
+        memset(color, -1, sizeof color);
+        for(int i=0; i<n; i++){
+            if(color[i] == -1){
+                if(!bfs(i, graph, color)){
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+};
+// TC--->O(n+n)
+// SC--->O(n*n)-->graph + O(n)+O(n)--color and queue
+
+//--------------------------------------------------
