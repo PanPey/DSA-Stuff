392016fc62f24bf7bcaf9224e307eb9d05b75674
Pankaj Pandey <52026014+PanPey@users.noreply.github.com>
2021-07-29 18:09:26 +0530

    Update 2Sum.cpp

--- SDE Sheet/2Sum.cpp
+++ SDE Sheet/2Sum.cpp
@@ -19,31 +19,3 @@ public:
 
 
 
-// two pointer apprach ---- TC---> O(n*log*n) with O(1) space
-class Solution {
-public:
-    
-    vector<int> twoSum(vector<int>& nums, int target) {
-     
-        sort(nums.begin(), nums.end());
-        
-        int i=0, j=nums.size()-1;
-        
-        while(i<j){
-            if(nums[i]+nums[j] == target){
-                return {i, j};
-            }
-            else if(nums[i]+nums[j]>target)
-            {
-                j--;
-            }
-               else
-               {
-                   i++;
-               }
-        }
-        
-        return {};
-        
-    }
-};
