9d736be1123fe184186daf9ab620add3d7018352
Pankaj Pandey <52026014+PanPey@users.noreply.github.com>
2021-08-17 15:05:34 +0530

    Update FistMissingPositive.cpp

--- SDE Sheet/FistMissingPositive.cpp
+++ SDE Sheet/FistMissingPositive.cpp
@@ -17,3 +17,27 @@ public:
 
 // TC ---> O(N)
 // SC ---> O(N)
+
+
+// Efficient Sol
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        
+        for(int i=0; i<n; i++){
+            while(nums[i]>0 && nums[i]<=n && nums[nums[i]-1]!=nums[i]){
+                swap(nums[i], nums[nums[i]-1]);
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            if(nums[i]!=i+1)
+                return i+1;
+        }
+        return n+1;
+    }
+};
+// TC--->O(n)
+// SC--->O(1)
