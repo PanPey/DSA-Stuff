abd32c51d4662c2c11d40901793a1a7d0ab644eb
Pankaj Pandey <52026014+PanPey@users.noreply.github.com>
2021-08-17 15:06:59 +0530

    Create First missing Positive.cpp

---
+++ LC Imp Problems/Arrays/First missing Positive.cpp
@@ -0,0 +1,27 @@
+/*41. First Missing Positive
+Hard
+Given an unsorted integer array nums, return the smallest missing positive integer.
+
+You must implement an algorithm that runs in O(n) time and uses constant extra space.
+*/
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        
+        for(int i=0; i<n; i++){
+            while(nums[i]>0 && nums[i]<=n && nums[nums[i]-1]!=nums[i]){
+                swap(nums[i], nums[nums[i]-1]);
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            if(nums[i]!=i+1)
+                return i+1;
+        }
+        return n+1;
+    }
+};
+// TC--->O(n)
+// SC--->O(1)
